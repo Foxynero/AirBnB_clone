7ba0c382374bbdbf0002636242753ede618e1b5b
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-03-12 08:41:57 +0000

    AirBnB clone

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,120 +1,79 @@
 #!/usr/bin/python3
-"""
-Module containing the unit test for file storage
-"""
+"""Module that holds tests for file_storage.py"""
 import unittest
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+import json
 import models
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import os
 
 
 class TestFileStorage(unittest.TestCase):
-    """ Defines the unit tests for file_storage """
+    """Class to test file_storage"""
 
     def setUp(self):
-        """
-        setUp function that runs before every test
-        """
+        """Sets up the class test"""
+
+        self.b1 = BaseModel()
+        self.a1 = Amenity()
+        self.c1 = City()
+        self.p1 = Place()
+        self.r1 = Review()
+        self.s1 = State()
+        self.u1 = User()
         self.storage = FileStorage()
-        self.storage.reload()
-        self.name_mapping = {
-            'BaseModel': BaseModel
-        }
+        if os.path.exists("file.json"):
+            pass
+        else:
+            os.mknod("file.json")
 
     def tearDown(self):
-        """
-        runs after every test method
-        """
-        all_objs = self.storage.all()
-        all_objs.clear()
-        self.storage.save()
+        """Tears down the testing environment"""
 
-    def test_all_method(self):
-        """
-        tests if storage.all() returns a dictionary type
-        """
-        self.a = BaseModel()
-        self.a.save()
-        self.assertIsInstance(self.storage.all(), dict)
+        del self.b1
+        del self.a1
+        del self.c1
+        del self.p1
+        del self.r1
+        del self.s1
+        del self.u1
+        del self.storage
+        if os.path.exists("file.json"):
+            os.remove("file.json")
 
-    def test_if_obj_in_dictionary(self):
-        """
-        Tests if the values inside __objects
-        are objs(e.g BaseModel)
-        """
-        self.new = BaseModel()
-        class_name = self.new.__class__.__name__
-        key = class_name + "." + self.new.id
-        self.assertIsInstance(
-            self.storage.all()[key], self.name_mapping[class_name])
+    def test_storage_all(self):
+        """Checks storage.all() is not empty"""
 
-    def test_instance_creation(self):
-        """
-        Tests if the key with the format <classname>.<id> can be found
-        in storage.all()
-        """
-        self.new = BaseModel()
-        self.key = "{}.{}".format(self.new.__class__.__name__, self.new.id)
-        self.assertIn(self.key, self.storage.all().keys())
+        self.assertIsNotNone(self.storage.all())
 
-    def test_storage_file_path_str(self):
-        """
-        Checks if the private class attribute is a str
-        """
+    def test_storage_all_contents(self):
+        """Checks to make sure storage.all() contains instances"""
 
-        self.assertIsInstance(self.storage._FileStorage__file_path, str)
+        self.assertIn("{}.{}".format(self.b1.__class__.__name__, self.b1.id),
+                      models.storage.all())
+        self.assertIn("{}.{}".format(self.a1.__class__.__name__, self.a1.id),
+                      models.storage.all())
+        self.assertIn("{}.{}".format(self.c1.__class__.__name__, self.c1.id),
+                      models.storage.all())
+        self.assertIn("{}.{}".format(self.p1.__class__.__name__, self.p1.id),
+                      models.storage.all())
+        self.assertIn("{}.{}".format(self.r1.__class__.__name__, self.r1.id),
+                      models.storage.all())
+        self.assertIn("{}.{}".format(self.s1.__class__.__name__, self.s1.id),
+                      models.storage.all())
+        self.assertIn("{}.{}".format(self.u1.__class__.__name__, self.u1.id),
+                      models.storage.all())
 
-    def test_save_method(self):
-        """
-        Tests if the save functionality of the class works
-        """
-        import json
-        self.new = BaseModel()
-        self.new.save()
-        with open('file.json', 'r') as file:
-            json_string = file.read()
-        key = "{}.{}".format('BaseModel', self.new.id)
-        ref_dict = {key: self.new.to_dict()}
-        dict_after_load = json.loads(json_string)
-        self.assertEqual(ref_dict, dict_after_load)
+    def test_storage_all_type(self):
+        """Checks the type of storage.all()"""
 
-    def test_reload_json_file_no_existo(self):
-        """
-        Tests if reload raises a FileNotFoundError when
-        the json file is removed
-        """
-        import os
+        self.assertEqual(dict, type(self.storage.all()))
 
-        os.remove("file.json")
-        self.assertRaises(FileNotFoundError, self.storage.reload())
-
-    def test_reload_json_empty(self):
-        """
-        Tests if reload loads an empty dictionary
-        """
-
-        self.storage.reload()
-        self.assertEqual(len(self.storage.all()), 0)
-
-    def test_reload_json_has_something(self):
-        """
-        Tests if the json file is not empty and reload
-        loads a key and a dictionary into __objects
-        """
-        self.new = BaseModel()
-        self.new.save()
-        self.key = "{}.{}".format('BaseModel', self.new.id)
-
-        self.storage.reload()
-        self.all_objs = self.storage.all()
-
-        self.assertIsInstance(self.all_objs[self.key], BaseModel)
-
-    def test_new_method(self):
-        """
-        Tests if the new method is adding instances to the storage dictionary
-        """
-
-        self.a = BaseModel()
-        self.key = 'BaseModel.{}'.format(self.a.id)
-        self.assertIn(self.key, self.storage.all())
+if __name__ == "__main__":
+    unittest.main()
