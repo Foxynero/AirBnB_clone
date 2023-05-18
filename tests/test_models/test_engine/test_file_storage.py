d6db5f8873c22463abf965046dd291c9a3481d5f
Foxynero <113718153+Foxynero@users.noreply.github.com>
2023-05-18 14:39:53 +0000

    Update test_file_storage.py

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,79 +1,96 @@
 #!/usr/bin/python3
-"""Module that holds tests for file_storage.py"""
+"""test for file storage"""
 import unittest
+import pep8
 import json
-import models
-from models.engine.file_storage import FileStorage
+import os
 from models.base_model import BaseModel
-from models.amenity import Amenity
+from models.user import User
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
-from models.user import User
-import os
+from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
-    """Class to test file_storage"""
+    '''this will test the FileStorage'''
 
-    def setUp(self):
-        """Sets up the class test"""
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.user = User()
+        cls.user.first_name = "Kev"
+        cls.user.last_name = "Yo"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = FileStorage()
 
-        self.b1 = BaseModel()
-        self.a1 = Amenity()
-        self.c1 = City()
-        self.p1 = Place()
-        self.r1 = Review()
-        self.s1 = State()
-        self.u1 = User()
-        self.storage = FileStorage()
-        if os.path.exists("file.json"):
-            pass
-        else:
-            os.mknod("file.json")
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down"""
+        del cls.user
 
     def tearDown(self):
-        """Tears down the testing environment"""
-
-        del self.b1
-        del self.a1
-        del self.c1
-        del self.p1
-        del self.r1
-        del self.s1
-        del self.u1
-        del self.storage
-        if os.path.exists("file.json"):
+        """teardown"""
+        try:
             os.remove("file.json")
+        except Exception:
+            pass
 
-    def test_storage_all(self):
-        """Checks storage.all() is not empty"""
+    def test_pep8_FileStorage(self):
+        """Tests pep8 style"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(p.total_errors, 0, "fix pep8")
 
-        self.assertIsNotNone(self.storage.all())
+    def test_all(self):
+        """tests if all works in File Storage"""
+        storage = FileStorage()
+        obj = storage.all()
+        self.assertIsNotNone(obj)
+        self.assertEqual(type(obj), dict)
+        self.assertIs(obj, storage._FileStorage__objects)
 
-    def test_storage_all_contents(self):
-        """Checks to make sure storage.all() contains instances"""
+    def test_new(self):
+        """test when new is created"""
+        storage = FileStorage()
+        obj = storage.all()
+        user = User()
+        user.id = 123455
+        user.name = "Kevin"
+        storage.new(user)
+        key = user.__class__.__name__ + "." + str(user.id)
+        self.assertIsNotNone(obj[key])
 
-        self.assertIn("{}.{}".format(self.b1.__class__.__name__, self.b1.id),
-                      models.storage.all())
-        self.assertIn("{}.{}".format(self.a1.__class__.__name__, self.a1.id),
-                      models.storage.all())
-        self.assertIn("{}.{}".format(self.c1.__class__.__name__, self.c1.id),
-                      models.storage.all())
-        self.assertIn("{}.{}".format(self.p1.__class__.__name__, self.p1.id),
-                      models.storage.all())
-        self.assertIn("{}.{}".format(self.r1.__class__.__name__, self.r1.id),
-                      models.storage.all())
-        self.assertIn("{}.{}".format(self.s1.__class__.__name__, self.s1.id),
-                      models.storage.all())
-        self.assertIn("{}.{}".format(self.u1.__class__.__name__, self.u1.id),
-                      models.storage.all())
+    def test_reload_filestorage(self):
+        """
+        tests reload
+        """
+        self.storage.save()
+        Root = os.path.dirname(os.path.abspath("console.py"))
+        path = os.path.join(Root, "file.json")
+        with open(path, 'r') as f:
+            lines = f.readlines()
+        try:
+            os.remove(path)
+        except Exception:
+            pass
+        self.storage.save()
+        with open(path, 'r') as f:
+            lines2 = f.readlines()
+        self.assertEqual(lines, lines2)
+        try:
+            os.remove(path)
+        except Exception:
+            pass
+        with open(path, "w") as f:
+            f.write("{}")
+        with open(path, "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(self.storage.reload(), None)
 
-    def test_storage_all_type(self):
-        """Checks the type of storage.all()"""
-
-        self.assertEqual(dict, type(self.storage.all()))
 
 if __name__ == "__main__":
     unittest.main()
